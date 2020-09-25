2a8ef30fef1e6d4131b89ed1cefed7b909a0ce79
emigrated22 <70367569+emigrated22@users.noreply.github.com>
2020-09-26 03:31:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def snake():
 
-   return render_template("index.html")'
+   return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
